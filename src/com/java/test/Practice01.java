bbabfa5b8eb7914e8e9996180b70e2506b196baa
yoonsoo <yunsoo0510@daum.net>
2019-12-04 19:30:40 +0900

    19:30

--- src/com/java/test/Practice01.java
+++ src/com/java/test/Practice01.java
@@ -7,14 +7,12 @@ public class Practice01 {
 		System.out.println("start");
 		System.out.println("start2");
 
-
-
+		System.out.println("test");
 		System.out.println("im N!");
 
 		System.out.println("im N1111!");
 		System.out.println("im N2222!");
 
-		
-	}	
+	}
 
 }
